1e2793b207a5196521eb17192a31908d9436babf
Chris Henry <cdhenry@gmail.com>
2018-04-22 00:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function logDriverNames(drivers){
   return drivers.forEach(function(el,i,arr) { console.log(el.name); });
 }
+
+function logDriversByHometown(drivers, hometown){
+  return drivers.forEach(function(el,i,arry) { if el.hometown === hometown ? {console.log(el.name)}; });
+}
