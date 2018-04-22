8f6aac565bf7f7eb6220b11aadf26334f0f56ef2
Chris Henry <cdhenry@gmail.com>
2018-04-22 00:30:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
 function logDriverNames(drivers){
-  return drivers.forEach(function(el,i,arr) { console.log(el.name); });
+  return drivers.forEach(function(el, i, arr) { console.log(el.name); });
 }
 
 function logDriversByHometown(drivers, hometown){
-  return drivers.forEach(function(el,i,arry) { if el.hometown === hometown {console.log(el.name); } });
+  return drivers.forEach(function(el, i ,arry) {
+    if el.hometown === hometown {
+      console.log(el.name);
+    }
+  });
 }
